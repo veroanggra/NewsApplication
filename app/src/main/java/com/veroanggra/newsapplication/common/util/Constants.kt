084f1098e3adfe1010de486d25d480650dda63a2
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-04-12 16:13:34 +0700

    Update Constants.kt

--- app/src/main/java/com/veroanggra/newsapplication/common/util/Constants.kt
+++ app/src/main/java/com/veroanggra/newsapplication/common/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.veroanggra.newsapplication.common.util
 
 object Constants {
-    const val API_KEY = "d7525c19bc0f4a928614e46943fb20e7"
+    const val API_KEY = "Your APIkey"
     const val BASE_URL = "https://newsapi.org"
     const val SEARCH_NEWS_TIME_DELAY = 500L
     const val QUERY_PAGE_SIZE = 20
